0071077a92ccb736aceb767051f289a9c53d9b7a
Sandeeeepp <sandydorge2313@gmail.com>
2022-09-26 01:22:47 +0530

    changes made from personal editor

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+
+
+function updateRate()
+{
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal=document.getElementById("principal").value;
+    if(principal<=0 || principal==null){
+alert("Enter a positive number: ");
+    }
+var rate=document.getElementById("rate").value;
+var years=document.getElementById("years").value;
+
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+ var p='If you deposit '+principal+',\<br\>'+'at an interest rate of '+rate+'.\<br\>'+'You will recieve an amount of '+interest+',\<br\>'+'in the year '+year
+ document.getElementById('result').innerHTML=p;   
     
 }
         
